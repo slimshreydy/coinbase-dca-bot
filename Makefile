393a048bd9bdf6b7fae3304117175c8e2cc70aa9
Shrey Gupta <shreygupta97@gmail.com>
2024-01-15 15:44:16 -0500

    Fix GHA deploys, add linting

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 setup-ci:
-	pip install modal
+	pip install -r requirements.txt
 
 setup:
 	pip install -r requirements.txt
 
 run:
 	modal run main.py
+
+lint:
+	black -l 120 .
+	pylint .
 
 deploy:
 	modal deploy main.py
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import modal
 # TODO: Change stub name to a new, unique namespace.
 stub = modal.Stub("example-get-started")
 
+
 @stub.function()
 @modal.web_endpoint()
 def square(x):
-  print("This code is running on a remote worker!")
-  return x**2
+    print("This code is running on a remote worker!")
+    return x**2
+
 
 @stub.local_entrypoint()
 def main():
-  number = 4
-  print(f"The square of {number} is {square.remote(number)}.")
+    number = 4
+    print(f"The square of {number} is {square.remote(number)}.")
